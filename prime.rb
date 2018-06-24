6e8fe862e891c13d4d2e40e1bab3ef3f0b285cb0
Emily Wessel <ettl02@gmail.com>
2018-06-24 17:00:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 # Add  code here!
 def prime?(integer)
- if integer.prime?
-   return true
- else
-   return false
- end
- end
+ if integer < 0 || integer == 0 || integer == 1
+   false 
+ else 
+   (2..integer-1).to_a.all? do |factor|
+     integer % factor != 0
+   end 
+ end 
+ end 
+  
